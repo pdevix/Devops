bd09024b2140317a805fe5efb75e9870b4b73c38
pdevix <114137461+pdevix@users.noreply.github.com>
2022-09-22 16:25:28 +0530

    Update calc_pi.py

--- calc_pi.py
+++ calc_pi.py
@@ -1,20 +1,3 @@
 import sys
-from fractions import Fraction
 
-n = 0
-arr = []
-def calc_nvalue(n):
-    return (n+2)
-
-def pi_calc(n):
-    for i in range (0,4):
-        n = calc_nvalue(n)
-        denominator = n*(n+1)*(n+2)
-        val = divmod(4,denominator)
-        tmp = Fraction(4,denominator)
-        arr.insert(i,float(tmp))
-    value = 3 + arr[0] - arr[1] + arr[2] - arr[3]
-    return value 
-    
-pi_value = pi_calc(n)
-print("Pi value is ",pi_value)
+echo "Hello world"
